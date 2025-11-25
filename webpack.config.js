1ddd8cc59d9b974baf9de293925fda2c9522554b
mksigrest <mksigrest@gmail.com>
2025-11-24 19:58:15 -0500

    bugfix lean, added viewer to be bundled

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: {
         app: './client/maker.jsx',
         login: './client/login.jsx',
+        viewer: './client/viewer.jsx',
     },
     module: {
         rules: [
